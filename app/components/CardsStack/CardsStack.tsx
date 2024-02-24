7f1296bb1d2868785f8b27e3dc1d5ae54c329d37
Rithikeshh <rithikeshh@gmail.com>
2024-02-25 03:20:11 +0530

    update

--- app/components/CardsStack/CardsStack.tsx
+++ app/components/CardsStack/CardsStack.tsx
@@ -19,14 +19,9 @@ const cards = [
 ]
 const miniProjects = [
     {
-      name: 'Pokemon World',
-      image: pokemon.src, // url of image because Avatar doesn't support src as object.
-      liveUrl: 'https://rithikeshh.github.io/Pokemon-world/'
-    },
-    {
-      name: 'Movie Deck',
-      image: moviedeck.src, // url of image because Avatar doesn't support src as object.
-      liveUrl: 'https://rithikeshh.github.io/Movie-Deck-Project-Buidling---JS-Project-Building-Session-HTML-CSS---In-Cla---kj07fu39n86e/'
+      name: 'Todo List',
+      image: todo.src, // url of image because Avatar doesn't support src as object.
+      liveUrl: 'https://rithikeshh.github.io/Todo-list/'
     },
     {
       name: 'Weather App',
@@ -34,9 +29,14 @@ const miniProjects = [
       liveUrl: 'https://rithikeshh.github.io/weather-app/'
     },
     {
-      name: 'Todo List',
-      image: todo.src, // url of image because Avatar doesn't support src as object.
-      liveUrl: 'https://rithikeshh.github.io/Todo-list/'
+      name: 'Movie Deck',
+      image: moviedeck.src, // url of image because Avatar doesn't support src as object.
+      liveUrl: 'https://rithikeshh.github.io/Movie-Deck-Project-Buidling---JS-Project-Building-Session-HTML-CSS---In-Cla---kj07fu39n86e/'
+    },
+    {
+      name: 'Pokemon World',
+      image: pokemon.src, // url of image because Avatar doesn't support src as object.
+      liveUrl: 'https://rithikeshh.github.io/Pokemon-world/'
     },
   ]
 // These two are just helpers, they curate spring data, values that are later being interpolated into css
@@ -67,7 +67,7 @@ function Deck() {
     api.start(i => {
       if (index !== i) return // We're only interested in changing spring-data for the current spring
       const isGone = gone.has(index)
-      const x = isGone ? (400) * dir : down ? mx : 0 // When a card is gone it flys out left or right, otherwise goes back to zero
+      const x = isGone ? (448) * dir : down ? mx : 0 // When a card is gone it flys out left or right, otherwise goes back to zero
       const rot = mx / 100 + (isGone ? dir * 10 * velocity : 0) // How much the card tilts, flicking it harder makes it rotate faster
       const scale = down ? 1.1 : 1 // Active cards lift up a bit
       return {
